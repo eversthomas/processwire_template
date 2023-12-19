553bcab476a69d113365b57b7b65a38033aee39f
Thomas <evers.thomas@gmail.com>
2023-12-19 15:16:35 +0100

    Update style.php

--- style.php
+++ style.php
@@ -1,13 +1,15 @@
 <?php
 
 require_once "scripts/scssphp/scss.inc.php"; // Pfad zum scssphp-Verzeichnis
-use ScssPhp\ScssPhp\Compiler;
+use ScssPhp\ScssPhp\Compiler; // soll den Compiler nutzen
+use ScssPhp\ScssPhp\OutputStyle; // soll ermöglichen, den Output Style zu nutzen
 
-$scss = new Compiler();
+$scss = new Compiler(); // startet neuen Compiler
 $scss->setImportPaths("styles/"); // Pfad zum Verzeichnis deiner SCSS-Dateien
+$scss->setOutputStyle(OutputStyle::COMPRESSED); // Setzt den Ausgabestil auf komprimiert
 
 // hole die Haupt scss datei und kompiliere sie
 $css = $scss->compile('@import "main.scss";');
 
-// schreibe eine neue css datei (unminimiert)
+// schreibe eine neue css datei
 file_put_contents("styles/style.css", $css);
